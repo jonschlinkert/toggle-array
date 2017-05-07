e1334b499adf367348541ae53aaac9708b507ab4
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-07 15:50:41 -0400

    adds `enable` and `disable` methods

--- index.js
+++ index.js
@@ -2,18 +2,41 @@
 
 var isObject = require('isobject');
 
-module.exports = function(arr, prop, idx) {
+function toggle(arr, prop, idx) {
+  var ele = arr[idx];
+  var init = isObject(ele) ? ele[prop] : false;
   var len = arr.length;
   var i = -1;
 
   while (++i < len) {
-    var ele = arr[i];
-    if (!isObject(ele)) continue;
-    if (i === idx) {
-      ele[prop] = true;
+    ele = arr[i];
+    if (!isObject(ele)) {
+      continue;
+    }
+    if (i !== idx) {
+      ele[prop] = !!init;
     } else {
-      ele[prop] = false;
+      ele[prop] = !init;
     }
   }
   return arr;
+}
+
+toggle.enable = function(arr, prop, i) {
+  return initial(arr, prop, i, false);
 };
+
+toggle.disable = function(arr, prop, i) {
+  return initial(arr, prop, i, true);
+};
+
+function initial(arr, prop, i, init) {
+  if (isObject(arr[i])) arr[i][prop] = init;
+  return toggle(arr, prop, i);
+}
+
+/**
+ * Expose `toggle`
+ */
+
+module.exports = toggle;
--- test.js
+++ test.js
@@ -2,16 +2,16 @@
 
 require('mocha');
 var assert = require('assert');
-var toggleArray = require('./');
+var toggle = require('./');
 
 describe('toggle-array', function() {
   it('should export a function', function() {
-    assert.equal(typeof toggleArray, 'function');
+    assert.equal(typeof toggle, 'function');
   });
 
   it('should add the given property to all objects', function() {
     var arr = [{}, {}, {}, {}, {}];
-    assert.deepEqual(toggleArray(arr, 'foo', 1), [
+    assert.deepEqual(toggle(arr, 'foo', 1), [
       { foo: false },
       { foo: true },
       { foo: false },
@@ -22,7 +22,7 @@ describe('toggle-array', function() {
 
   it('should skip non-objects', function() {
     var arr = [{}, 'nope', {}, {}, {}];
-    assert.deepEqual(toggleArray(arr, 'foo', 1), [
+    assert.deepEqual(toggle(arr, 'foo', 1), [
       { foo: false },
       'nope',
       { foo: false },
@@ -31,14 +31,54 @@ describe('toggle-array', function() {
     ]);
   });
 
-  it('should disable all values that are not the given index', function() {
-    var arr = [{foo: true}, {foo: true}, {foo: true}, {foo: true}, {foo: true}];
-    assert.deepEqual(toggleArray(arr, 'foo', 1), [
+  it('should toggle all values to the opposite of the given index', function() {
+    var arr1 = [{foo: true}, {foo: true}, {foo: true}, {foo: true}, {foo: true}];
+    assert.deepEqual(toggle(arr1, 'foo', 1), [
+      { foo: true },
+      { foo: false },
+      { foo: true },
+      { foo: true },
+      { foo: true }
+    ]);
+
+    var arr2 = [{foo: false}, {foo: false}, {foo: false}, {foo: false}, {foo: false}];
+    assert.deepEqual(toggle(arr2, 'foo', 1), [
       { foo: false },
       { foo: true },
       { foo: false },
       { foo: false },
       { foo: false }
+    ]);
+
+    var arr3 = [{foo: true}, {foo: false}, {foo: false}, {foo: true}, {foo: false}];
+    assert.deepEqual(toggle(arr3, 'foo', 1), [
+      { foo: false },
+      { foo: true },
+      { foo: false },
+      { foo: false },
+      { foo: false }
+    ]);
+  });
+
+  it('should disable all values that are not the given index', function() {
+    var arr = [{foo: true}, {foo: true}, {foo: true}, {foo: true}, {foo: true}];
+    assert.deepEqual(toggle.enable(arr, 'foo', 1), [
+      { foo: false },
+      { foo: true },
+      { foo: false },
+      { foo: false },
+      { foo: false }
+    ]);
+  });
+
+  it('should enable all values that are not the given index', function() {
+    var arr = [{foo: true}, {foo: true}, {foo: true}, {foo: true}, {foo: true}];
+    assert.deepEqual(toggle.disable(arr, 'foo', 1), [
+      { foo: true },
+      { foo: false },
+      { foo: true },
+      { foo: true },
+      { foo: true }
     ]);
   });
 });
